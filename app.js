875373e9ffb6adaed5731bb95e56199aa14725d9
Byron Baldwin <byronsbaldwin@gmail.com>
2019-02-24 11:52:38 -0600

    Finished Layout and Design

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
-let cells = document.querySelectorAll('Cell');
+let cells = document.querySelectorAll('.Cell');
 
 cells.forEach(function(cell) {
     cell.addEventListener("Click", cellClicked);
 });
 
-function cellClicked(e) {
+let wins = {
+    
+    top:        [1, 2, 3],
+    mid:        [4, 5, 6],
+    bot:        [7, 8, 9],
 
-    e.target.textcontent = 'Hello';
+    left:       [1, 4, 7],
+    center:     [2, 5, 8],
+    right :     [3, 6, 9],
+
+    leftDiag:   [1, 5, 9],
+    rightDiag:  [7, 5, 3]
 }
 
+ let players = [player1, player2];
+
+
+function selectPlayer(player1, player2) {
+    if (player1)
+}
