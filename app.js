2ef8c402c920730ca72f605e2414388d60a63702
Byron Baldwin <byronsbaldwin@gmail.com>
2019-05-11 13:48:47 -0500

    After a long hiatus from focusing on Hilton GM training, I am back on this lab

--- app.js
+++ app.js
@@ -1,26 +1,84 @@
-let cells = document.querySelectorAll('.Cell');
 
-cells.forEach(function(cell) {
-    cell.addEventListener("Click", cellClicked);
+let humanTurn = [];
+
+let computerTurn = [];
+
+let totalTurns = 0;
+
+let whichTurn = "";
+
+let checkWin = false;
+
+
+
+
+
+let resetTheGame = document.querySelectorAll('.resetGame');
+
+let boardCells = [
+    document.getElementsByClassName('cellTl')[0],
+    document.getElementsByClassName('cellTm')[0],
+    document.getElementsByClassName('cellTr')[0],
+    document.getElementsByClassName('cellMl')[0],
+    document.getElementsByClassName('cellMm')[0],
+    document.getElementsByClassName('cellMr')[0],
+    document.getElementsByClassName('cellBl')[0],
+    document.getElementsByClassName('cellBm')[0],
+    document.getElementsByClassName('cellBr')[0],
+];
+
+let winCombo = [
+    [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,4],
+    [3,6,9], [1,5,9], [3,5,7]
+]
+
+resetTheGame.forEach(function(resetGame) {
+    resetGame.addEventListener('click', gameReset);
 });
 
-let wins = {
+function gameReset() {
+    for (let i = 0; i < boardCells.length; i++) {
+        boardCells[i].innerHTML = '';
+    }
+}
+
+boardCells.forEach(function(gameboard) {
+    gameboard.addEventListener('click', cellClicked);
+})
+
+function cellClicked(e) {
+    if (totalTurns === 0 && !checkWin) {
+        alert('Tie Game :/');
+        setTimeout(function() {
+            gameReset();
+        }, 1000);
+    }
+
+    else if (whichTurn === 'Human'  || totalTurns === 0) {
+        humanTurn();
+    } else if (whichTurn === 'computer') {
+        setTimeout(function() {
+            computerTurn();
+        }, 200);
+    }
+}
+
+function human() {
+    for (let i = 0; i < boardCells.length; i++) {
+        boardCells[i]('click', cellClicked() {
+            if (
+                computerTurn.indexOf(Number(this.id)) !== -1 ||
+                humanTurn.indexOf(Number(this.id)) !== -1 ||
+            ) {
+                return;
+            }
+
+            document.getElementsByClassName('gameBoard')[0].classList.add('clicked');
+            humanTurn.push(Number(this.id));
+            nextTurn('computer', humanTurn);
+
+        });
+    }
+}
+
     
-    top:        [1, 2, 3],
-    mid:        [4, 5, 6],
-    bot:        [7, 8, 9],
-
-    left:       [1, 4, 7],
-    center:     [2, 5, 8],
-    right :     [3, 6, 9],
-
-    leftDiag:   [1, 5, 9],
-    rightDiag:  [7, 5, 3]
-}
-
- let players = [player1, player2];
-
-
-function selectPlayer(player1, player2) {
-    if (player1)
-}
