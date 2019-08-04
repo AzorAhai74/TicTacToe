9414b1db10e6deaba14463d428dd6dc15b76767f
Byron Baldwin <byronsbaldwin@gmail.com>
2019-08-04 11:40:03 -0500

    Progress

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ cells.forEach(function(cell) {
 });
 
 function startGame() {
-    e.target.innerHTML = 'X';
-    document.getElementById('message').innerHTML = 'X takes their Turn';
+    e.target.textContent = 'You are X';
+    alert(e.target.textContent);
 };
 
 
@@ -43,7 +43,6 @@ function toggle() {
     } else {
         currentTurn = 0;
     }
-    document.getElementById('message').innerHTML = players[currentTurn] + " 's Turn";
 }
 
 
@@ -64,8 +63,7 @@ function checkWinner(e) {
             setTimeout(function() {
                 alert('X Wins the Game!');
             }, 100);
-            document.getElementById('message').innerHTML = 'Game Over';
-    } else if
+    } else if 
         (document.getElementById('topLeft').innerHTML === 'O' && document.getElementById('topCenter').innerHTML === 'O' && document.getElementById('topRight').innerHTML === 'O' ||
         document.getElementById('middleLeft').innerHTML === 'O' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('middleRight').innerHTML === 'O' ||
         document.getElementById('bottomLeft').innerHTML === 'O' && document.getElementById('bottomCenter').innerHTML === 'O' && document.getElementById('bottomRight').innerHTML === 'O' ||
@@ -74,37 +72,42 @@ function checkWinner(e) {
         document.getElementById('topRight').innerHTML === 'O' && document.getElementById('middleRight').innerHTML === 'O' && document.getElementById('bottomRight').innerHTML === 'O' ||
         document.getElementById('topLeft').innerHTML === 'O' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('bottomRight').innerHTML === 'O' ||
         document.getElementById('topRight').innerHTML === 'O' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('middleRight').innerHTML === 'O')
-        {
+        
 
             gameOver = true;
             setTimeout(function() {
                 alert('O Wins the Game!');
             }, 100);
-            document.getElementById('message').innerHTML = 'Game Over';
-
+        {
     }
 };
 
 function checkDraw(e) {
-    if (document.getElementById([1]) === 'X' || document.getElementById([1]) === 'O' &&
-    document.getElementById([2]) === 'X' || document.getElementById([2]) === 'O' &&
-    document.getElementById([3]) === 'X' || document.getElementById([3]) === 'O' &&
-    document.getElementById([4]) === 'X' || document.getElementById([4]) === 'O' &&
-    document.getElementById([5]) === 'X' || document.getElementById([5]) === 'O' &&
-    document.getElementById([6]) === 'X' || document.getElementById([5]) === 'O' &&
-    document.getElementById([7]) === 'X' || document.getElementById([7]) === 'O' &&
-    document.getElementById([8]) === 'X' || document.getElementById([8]) === 'O' &&
-    document.getElementById([9]) === 'X' || document.getElementById([9]) === 'O') { 
-    {
+    if (document.getElementById('topLeft').innerHTML === 'X' && document.getElementById('topCenter').innerHTML === 'O' && document.getElementById('topRight').innerHTML === 'X' ||
+        document.getElementById('topLeft').innerHTML === 'O' && document.getElementById('topCenter').innerHTML === 'X' && document.getElementById('topRight').innerHTML === 'O' ||
+        document.getElementById('middleLeft').innerHTML === 'X' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('middleRight').innerHTML === 'X' ||
+        document.getElementById('middleLeft').innerHTML === 'O' && document.getElementById('middleCenter').innerHTML === 'X' && document.getElementById('middleRight') === 'O' ||
+        document.getElementById('bottomLeft').innerHTML === 'X' && document.getElementById('bottomCenter').innerHTML === 'O' && document.getElementById('bottomRight').innerHTML === 'X' ||
+        document.getElementById('bottomLeft').innerHTML === 'O' && document.getElementById('bottomCenter').innerHTML === 'X' && document.getElementById('bottomRight').innerHTML === 'O' ||
+        document.getElementById('topLeft').innerHTML === 'X' && document.getElementById('middleLeft').innerHTML === 'O' && document.getElementById('bottomLeft').innerHTML === 'X' ||
+        document.getElementById('topLeft').innerHTML === 'O' && document.getElementById('middleLeft').innerHTML === 'X' && document.getElementById('bottomLeft').innerHTML === 'O' ||
+        document.getElementById('topMiddle').innerHTML === 'X' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('bottomMiddle').innerHTML === 'X' ||
+        document.getElementById('topMiddle').innerHTML === 'O' && document.getElementById('middlecenter').innerHTML === 'X' && document.getElementById('bottomMiddle').innerHTML === 'O' ||
+        document.getElementById('topRight').innerHTML === 'X' && document.getElementById('midddleRight').innerHTML === 'O' && document.getElementById('bottomRight').innerHTML === 'X' ||
+        document.getElementById('topRight').innerHTML === 'O' && document.getElementById('middleRight').innerHTML === 'X' && document.getElementById('bottomRight').innerHTML === 'O' ||
+        document.getElementById('topLeft').innerHTML === 'X' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('bottomRight').innerHTML === 'X' ||
+        document.getElementById('topLeft').innerHTML === 'O' && document.getElementById('middleCenter').innerHTML === 'X' && document.getElementById('bottomRight').innerHTML === 'O' ||
+        document.getElementById('topRight').innerHTML === 'X' && document.getElementById('middleCenter').innerHTML === 'O' && document.getElementById('bottomLeft').innerHTML === 'X' ||
+        document.getElementById('topRight').innerHTML === 'O' && document.getElementById('middleCenter').innerHTML === 'X' && document.getElementById('bottomRight').innerHTML === 'O')
+        {
     
             draw = true;
             setTimeout(function() {
                 alert('Tie Game!');
             }, 100);
-            document.getElementById('nessage').innerHTML = 'Game Over';
-        }  
-    }
+    }  
 };
+
 
 
 
