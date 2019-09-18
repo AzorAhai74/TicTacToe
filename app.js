8608cdbb92be187ebd925266c77d1e643538a215
byron baldwin <byronbaldwin@byrons-MacBook-Pro.local>
2019-09-18 17:26:06 -0500

    code wioth math.random for archives

--- app.js
+++ app.js
@@ -1,73 +1,65 @@
-let players = ['X', 'O'];
-let currentTurn;
+let currentTurn = 0;
 
-let cpu = 'O';
+
+let gameOver = false;
+
+let places = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'];
 
 
 let cells = document.querySelectorAll('.cell');
 
 
-
 cells.forEach(function(cell) {
-    cell.addEventListener('click', cellClicked) 
-});
-
+    cell.addEventListener('click', cellClicked);
+})
 
 
 
 
 function cellClicked(e) {
-	cells.forEach(function(cell){
-	if (cell.textContent == '') {
-	e.target.textContent = 'X';	
-	switchPlayer();
-	} 
-	});
-	var blankCells = [];
-	var random;
-	for (var i = 1; i < 9; i++) {
-		if (cells[i].textContent = '') {
-			blankCells.push(cells[i]);
-		}
+    if (gameOver == true) {
+        clearBoard();
+        gameOver = false;
+        return;
+    }
+	if (e.target.textContent === 'X' || e.target.textContent === 'O') {
+		return;
 	}
-	if (cells.textContent == '') {
-		blankCells.push(cells);
-	};
-	cells.forEach(function(cell) {
-		if (cell.textContent == '') {
-			blankCells.push(cell);	
-			random = Math.ceil(Math.random() * blankCells.length) - 1;
-			blankCells[random].textContent = 'O';
-			switchPlayer();
-		}
-	});	
 	
-};
+	
+}
 
-function switchPlayer() {
-	if (currentTurn == 'X') {
-		currentTurn = 'O';
-	} else {
-		currentTurn = 'X';
-	}
+	
+		
+
+
+
+function computerTurn() {
+	let cpuMove = Math.floor(Math.random() * places.length);
+    let cpuPlayedTileId = places[cpuMove];
+    let pos = document.getElementById(cpuPlayedTileId);
+	places.splice(places.indexOf(cpuPlayedTileId), 1);
+	pos.textContent = 'O';
+	switchTurn();
 }
 
 
+
+
+
+
+
+function switchTurn() {
+    if (currentTurn === 0) {
+        currentTurn = 1;
+    } else {
+    currentTurn = 0;
+    }
+}
 
 function clearBoard() {
-	location.reload();
+    location.reload();
 }
-
-function checkRow() {
-	winner(document.getElementById('c1'), document.getElementById('c2'), document.getElementById('c3'));
-	winner(document.getElementById('c4'), document.getElementById('c5'), document.getElementById('c6'));
-	winner(document.getElementById('c7'), document.getElementById('c8'), document.getElementById('c9'));
-	winner(document.getElementById('c1'), document.getElementById('c4'), document.getElementById('c7'));
-	winner(document.getElementById('c2'), document.getElementById('c5'), document.getElementById('c8'));
-	winner(document.getElementById('c3'), document.getElementById('c6'), document.getElementById('c9'));
-	winner(document.getElementById('c1'), document.getElementById('c5'), document.getElementById('c9'));
-	winner(document.getElementById('c3'), document.getElementById('c5'), document.getElementById('c7'));
-};
 
 
 
