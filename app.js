c94da64eedec49e9bee5a9c998343eae7089603c
byron baldwin <byronbaldwin@byrons-MacBook-Pro.local>
2019-09-18 18:36:58 -0500

    win draw code

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let currentTurn = 0;
 
+let players = ['X', 'O'];
 
 let gameOver = false;
 
-let places = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9'];
+
 
 
 let cells = document.querySelectorAll('.cell');
@@ -14,40 +15,64 @@ cells.forEach(function(cell) {
 })
 
 
-
-
 function cellClicked(e) {
     if (gameOver == true) {
         clearBoard();
         gameOver = false;
-        return;
-    }
+		return;
+	}
 	if (e.target.textContent === 'X' || e.target.textContent === 'O') {
 		return;
 	}
-	
-	
-}
-
-	
-		
-
-
-
-function computerTurn() {
-	let cpuMove = Math.floor(Math.random() * places.length);
-    let cpuPlayedTileId = places[cpuMove];
-    let pos = document.getElementById(cpuPlayedTileId);
-	places.splice(places.indexOf(cpuPlayedTileId), 1);
-	pos.textContent = 'O';
+	e.target.textContent = players[currentTurn];
 	switchTurn();
 }
 
+function checkWinner(e) {
+	if (document.querySelector('c1').textContent === 'X' && document.querySelector('c2').textContent === 'X' && document.querySelector('c3') === 'X' ||
+		document.querySelector('c4').textContent === 'X' && document.querySelector('c5').textContent === 'X' && document.querySelector('c6') === 'X' ||
+		document.querySelector('c7').textContent === 'X' && document.querySelector('c8').textContent === 'X' && document.querySelector('c9') === 'X' ||
+		document.querySelector('c1').textContent === 'X' && document.querySelector('c3').textContent === 'X' && document.querySelector('c7') === 'X' ||
+		document.querySelector('c2').textContent === 'X' && document.querySelector('c5').textContent === 'X' && document.querySelector('c8') === 'X' ||
+		document.querySelector('c3').textContent === 'X' && document.querySelector('c6').textContent === 'X' && document.querySelector('c9') === 'X' ||
+		document.querySelector('c1').textContent === 'X' && document.querySelector('c5').textContent === 'X' && document.querySelector('c9') === 'X' ||
+		document.querySelector('c3').textContent === 'X' && document.querySelector('c5').textContent === 'X' && document.querySelector('c7') === 'X' ) {
+		{
+			gameOver = true;
+			setTimeout(function() {
+				alert('X Wins!');
+			}, 100);
+			document.getElementById('message').textContent = 'Game Over';
+		}
+	} else if 
+		(document.querySelector('c1').textContent === 'O' && document.querySelector('c2').textContent === 'O' && document.querySelector('c3') === 'O' ||
+		document.querySelector('c4').textContent === 'O' && document.querySelector('c5').textContent === 'O' && document.querySelector('c6') === 'O' ||
+		document.querySelector('c7').textContent === 'O' && document.querySelector('c8').textContent === 'O' && document.querySelector('c9') === 'O' ||
+		document.querySelector('c1').textContent === 'O' && document.querySelector('c3').textContent === 'O' && document.querySelector('c7') === 'O' ||
+		document.querySelector('c2').textContent === 'O' && document.querySelector('c5').textContent === 'O' && document.querySelector('c8') === 'O' ||
+		document.querySelector('c3').textContent === 'O' && document.querySelector('c6').textContent === 'O' && document.querySelector('c9') === 'O' ||
+		document.querySelector('c1').textContent === 'O' && document.querySelector('c5').textContent === 'O' && document.querySelector('c9') === 'O' ||
+		document.querySelector('c3').textContent === 'O' && document.querySelector('c5').textContent === 'O' && document.querySelector('c7') === 'O')
+		{
+			gameOver = true;
+			setTimeout(function() {
+				alert('O Wins!');
+			}, 100);
+			document.getElementById('message').textContent = 'Game Over';
+		}
+	} 
 
 
 
 
 
+		
+
+		 
+		
+				
+
+	
 
 function switchTurn() {
     if (currentTurn === 0) {
